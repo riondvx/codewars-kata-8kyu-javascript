c3236bc53ee30a186fcec875e68d5867142726bf
Rion <rioneffendy0001repo@gmail.com>
2024-07-18 22:57:54 +0700

    Create even_or_odd.js

---
+++ even_or_odd.js
@@ -0,0 +1,12 @@
+function evenOrOdd(number) {
+  if(number % 2 == 0) {
+    return "Even"
+  }
+  else {
+    return "Odd"
+  }
+}
+
+// function evenOrOdd(number) {
+//   return number % 2 == 0 ? "Even" : "Odd"
+// }
