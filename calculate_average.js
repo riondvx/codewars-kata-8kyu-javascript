af95af08dc7babd1589de8fc0541a2da11cce1fc
Rion <rioneffendy0001repo@gmail.com>
2024-07-25 22:10:20 +0700

    Update calculate_average.js

--- calculate_average.js
+++ calculate_average.js
@@ -1,9 +1,21 @@
+// Code Wars
+// https://www.codewars.com/kata/57a2013acf1fa5bfc4000921
+
 function findAverage(array) {
+  if(array.length == 0) {
+    return 0
+  }
+  
   var sum = 0
   
   for(let i = 0; i < array.length; i++) {
     sum += array[i]
   }
   
-  return sum / (array.length || 1) // 0 || 1 => 1
+  return sum / array.length
 }
+
+
+// function findAverage(array) {
+//   return array.length == 0 ? 0 : array.reduce((total, v)=> total + v) / array.length
+// }
